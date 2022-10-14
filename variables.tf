e4ddb645c04f45b9b678be6cdceeb2a400429ed7
andresogt <aogilt@gmail.com>
2022-10-14 11:53:01 -0500

    upload variables.tf

---
+++ variables.tf
@@ -0,0 +1,17 @@
+variable "vpc_cidr" {}
+
+variable "public_subnet1_cidr" {
+}
+
+variable "public_subnet2_cidr" {
+}
+
+variable "region" {
+    default = "us-east-1"
+}
+
+variable "instance_type" {}
+
+variable "key_name" {}
+
+variable "ami_id" {}
